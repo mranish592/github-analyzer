b5ac76014715d458c0127812bc430bd26cbffe58
Anish\ Gupta <mranish592@gmail.com>
2025-03-22 20:25:05 +0530

    feat: frontend init: react, typescript, shadcn, tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Analyze</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
